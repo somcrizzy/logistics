9e50ab49f9916dc57c6ac63e15e3b2500e83662f
somcrizzy <nnakwechris@gmail.com>
2024-03-19 13:42:31 +0100

    first work

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,15 @@ function App() {
   useEffect(() => {
     AOS.init();
   }, [])
-  
+
   return (
     <>
     <Header/>
    <Router>
     <Routes>
       <Route exact path='/' element={<Homepage/>} />
-      <Route exact path='/services' element={<Services/>} />
-      <Route exact path='/contact' element={<Contact/>} />
-      
+      <Route exact path='/services' element={<Services/>}/>
+      <Route exact path='/contact' element={<Contact/>} />      
       <Route exact path='/tracking' element={<Tracking/>} />
     </Routes>
    </Router>
